9d588e820d59c8f960b83023a2bfdf37b8bf20e1
dennvi1997 <dennvi1997@gmail.com>
2025-01-24 14:48:42 +0700

    Delete memberCoding directory

--- memberCoding/src/main/java/id/allianz/coding/memberCoding/service/MemberCodingClassService.java
+++
@@ -1,7 +0,0 @@
-package id.allianz.coding.memberCoding.service;
-
-import id.allianz.coding.memberCoding.entity.MemberCodingClass;
-
-public interface MemberCodingClassService {
-    void insertMember(MemberCodingClass entity);
-}
